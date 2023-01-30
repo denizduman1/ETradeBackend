05309264bdfabcbfebed006363e91be206f737cd
denizduman1 <ce.denizduman2@gmail.com>
2023-01-30 23:09:50 +0300

    #3- Asp.NET Core 6 + Angular İle Mini E-Ticaret | Onion Architecture Altyapısında İnşa - bitti

---
+++ Core/ETradeBackend.Application/Abstractions/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using ETradeBackend.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETradeBackend.Application.Abstractions
+{
+    public interface IProductService
+    {
+        List<Product> GetProducts();
+    }
+}
---
+++ Infrastructure/ETradeBackend.Persistence/ServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using ETradeBackend.Application.Abstractions;
+using ETradeBackend.Persistence.Concretes;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETradeBackend.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<IProductService, ProductService>();
+        }
+    }
+}
